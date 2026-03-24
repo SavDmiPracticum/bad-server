3fa0fa93ba0bd985b7b0a1e5687eb61ba0fe9b6c
Dima <savdmi@gmail.com>
2026-03-24 14:43:27 +0300

    test 1

---
+++ backend/src/utils/sanitizeInput.ts
@@ -0,0 +1,20 @@
+import sanitizeHtml from 'sanitize-html'
+
+export const sanitizeInput = (input: string) => {
+    const maxLength = 100
+
+        const cleanHtml = sanitizeHtml(input as string, {
+        allowedTags: [],
+        allowedAttributes: {},
+    })
+
+    const cleanedInput = cleanHtml.replace(/[.*+?^=!:${}()|\]\\]/g, '\\$&')
+
+    if (cleanedInput.length > maxLength) {
+        return cleanedInput.slice(0, maxLength)
+    } else {
+        return cleanedInput
+    }
+}
+
+export default sanitizeInput;
